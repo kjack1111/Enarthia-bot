c79fd2d6fa2065af3d8674d2dfe9c31e0504e69b
Kenneth Jack Gilchrist <kjackgilchrist@gmail.com>
2018-10-14 12:42:31 -0600

    Error handling, Changing stuff around

--- register.py
+++ register.py
@@ -1,8 +1,12 @@
 async def register(client, M):
-        await client.send_message(M.channel, f"Hello, <@{M.author.id}>! It looks like you're trying to register a new nation! Please give me the Name.")
-        M2 = await client.wait_for_message(timeout=30, author=M.author, channel=M.channel)
+    human = M.author
+    await client.send_message(M.channel, f"Hello, <@{human.id}>! It looks like you're trying to register a new nation! Please give me the Name.")
+    M2 = await client.wait_for_message(timeout=30, author=human, channel=M.channel)
+    if M2==None:
+        await client.send_message(M.channel, "The command timed out! Please try again.")
+    else:
         name = M2.content
-        print(name)
+        
 
 
 # await register.register(client, M)
