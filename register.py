1d0a2da256738d85c33f78e98134f109791032f5
Kenneth Jack Gilchrist <kjackgilchrist@gmail.com>
2018-10-22 19:54:18 -0600

    Lots of backstage work, preparing to develop the holy JSON writing function. Also, basis for the Gratules system implimented.

--- register.py
+++ register.py
@@ -9,6 +9,15 @@ async def register(client, M):
         return
     else:
         name = rname
+        
+    await client.send_message(M.channel, f"Thank you, <@{human.id}>! Can you please ping the user that you're registering the country for?")
+    rleader = await client.wait_for_message(timeout=15, author=human, channel=M.channel)
+    if rleader==None:
+        await util.timeout(client, M)
+        return
+    else:
+        leader = rleader.mentions[0]
+        
     await client.send_message(M.channel, f"Alright, <@{human.id}>. Please give me the Government type.")
     rgovt = await client.wait_for_message(timeout=15, author=human, channel=M.channel)
     if rgovt==None:
@@ -16,6 +25,15 @@ async def register(client, M):
         return
     else:
         govt = rgovt
+
+    await client.send_message(M.channel, f"Thank you, <@{human.id}>. Please put in A NUMBER for the starting population.")
+    rpopulation = await client.wait_for_message(timeout=15, author=human, channel=M.channel)
+    if rpopulation==None:
+        await util.timeout(client, M)
+        return
+    else:
+        population = int(rpopulation)
+        
         
 
 
